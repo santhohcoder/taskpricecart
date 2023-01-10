965e5db3ed145d401de8a8c3ae1de1f7ee9284c8
SANTHOSH KANNA <praddossanthosh@gmail.com>
2023-01-10 17:10:18 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { contentdata } from "./data"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="fulldiv">
+
+      {contentdata.details.map((res) => {
+        
+        return <div class="maindiv">
+          <div class="heading">
+            <Heading header={res.topic} cost={res.cost} />
+          </div>
+          <hr />
+          <div class="contentsdiv">
+            <ul>
+               {res.user.symbol=="T" ?<li> <i class="fa fa-check icon " aria-hidden="true"></i> {res.user.data}</li>:<li class="disabled"><i class="fa fa-times icon disabled" aria-hidden="true"></i> {res.user.data}</li>}
+               {res.storage.symbol=="T" ?<li> <i class="fa fa-check icon " aria-hidden="true"></i> {res.storage.data}</li>:<li class="disabled"><i class="fa fa-times icon disabled" aria-hidden="true"></i> {res.storage.data}</li>}
+               {res.public_projects.symbol=="T" ?<li> <i class="fa fa-check icon " aria-hidden="true"> </i>{res.public_projects.data}</li>:<li class="disabled"><i class="fa fa-times icon disabled" aria-hidden="true"> </i>{res.public_projects.data}</li>}
+               {res.access.symbol=="T" ?<li> <i class="fa fa-check icon " aria-hidden="true"></i> {res.access.data}</li>:<li class="disabled"><i class="fa fa-times icon disabled" aria-hidden="true"> </i>{res.access.data}</li>}
+               {res.private_projects.symbol=="T" ?<li> <i class="fa fa-check icon " aria-hidden="true"></i> {res.private_projects.data}</li>:<li class="disabled"><i class="fa fa-times icon disabled" aria-hidden="true"></i> {res.user.data}</li>}
+               {res.support.symbol=="T" ?<li> <i class="fa fa-check icon " aria-hidden="true"> </i>{res.support.data}</li>:<li class="disabled"><i class="fa fa-times icon disabled" aria-hidden="true"></i> {res.support.data}</li>}
+               {res.domain.symbol=="T" ?<li> <i class="fa fa-check icon " aria-hidden="true"></i> {res.domain.data}</li>:<li class="disabled"><i class="fa fa-times icon disabled" aria-hidden="true"></i> {res.domain.data}</li>}
+               {res.reports.symbol=="T" ?<li> <i class="fa fa-check icon " aria-hidden="true"></i> {res.reports.data}</li>:<li class="disabled"><i class="fa fa-times icon disabled" aria-hidden="true"></i> {res.reports.data}</li>}
+
+              {/* <li> {res.storage.symbol=="T" ? <i class="fa fa-check icon" aria-hidden="true"></i>:<i class="fa fa-times icon disabled" aria-hidden="true"></i>}{res.storage.data}</li> */}
+              {/* <li> {res.public_projects.symbol=="T" ? <i class="fa fa-check icon" aria-hidden="true"></i>:<i class="fa fa-times icon disabled" aria-hidden="true"></i>}{res.public_projects.data}</li>
+              <li> {res.access.symbol=="T" ? <i class="fa fa-check icon" aria-hidden="true"></i>:<i class="fa fa-times icon disabled" aria-hidden="true"></i>}{res.access.data}</li>
+              <li> {res.private_projects.symbol=="T" ? <i class="fa fa-check icon" aria-hidden="true"></i>:<i class="fa fa-times icon disabled" aria-hidden="true"></i>}{res.private_projects.data}</li>
+              <li> {res.support.symbol=="T" ? <i class="fa fa-check icon" aria-hidden="true"></i>:<i class="fa fa-times icon disabled" aria-hidden="true"></i>}{res.support.data}</li>
+              <li>{res.domain.symbol=="T" ? <i class="fa fa-check icon" aria-hidden="true"></i>:<i class="fa fa-times icon disabled" aria-hidden="true"></i>}{res .domain.data}</li>
+              <li>{res.reports.symbol=="T" ? <i class="fa fa-check icon" aria-hidden="true"></i>:<i class="fa fa-times icon disabled" aria-hidden="true"></i>}{res .reports.data}</li> */}
+            </ul>
+          </div>
+
+          <div class="buttons">
+            <button  class="buttonclass" type="button">BUTTON</button>
+          </div>
+        </div>
+      })}
+
+    </div >
+  );
+
+}
+
+const Heading = (props) => {
+  return (
+    <div>
+      <h3>{props.header}</h3>
+      <p> <b class="boldtext">${props.cost}</b>/month</p>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const contentdata = {
+    details: [
+      {
+        topic:"FREE",
+        cost:"0",
+        user: {symbol:"T", data:"Single User"},
+        storage: {symbol:"T",data:"5GB Storage"},
+        public_projects: {symbol:"T",data:"Unlimited Public Projects"},
+        access: {symbol:"T",data:"Community Access"},
+        private_projects:{symbol:"C",data: "Unlimited Private Projects"},
+        support: {symbol:"C",data:"Dedicated Phone Support"},
+        domain:{symbol:"C",data: "Free Subdomain"},
+        reports:{symbol:"C",data: "Monthly Status Reports"},
+      },
+      {
+        topic:"PLUS",
+        cost:"9",
+        user: {symbol:"T",data:"5 Users"},
+        storage:{symbol:"T",data: "50GB Storage"},
+        public_projects:{symbol:"T",data: "Unlimited Public Projects"},
+        access: {symbol:"T",data:"Community Access"},
+        private_projects:{symbol:"T",data: "Unlimited Private Projects"},
+        support:{symbol:"T",data: "Dedicated Phone Support"},
+        domain: {symbol:"T",data:"Free Subdomain"},
+        reports:{symbol:"C",data: "Monthly Status Reports"},
+      },
+      {
+        topic:"PRO",
+        cost:"49",
+        user:{symbol:"T",data: "Unlimited Users"},
+        storage:{symbol:"T",data: "150GB Storage"},
+        public_projects: {symbol:"T",data:"Unlimited Public Projects"},
+        access:{symbol:"T",data: "Community Access"},
+        private_projects:{symbol:"T",data: "Unlimited Private Projects"},
+        support: {symbol:"T",data:"Dedicated Phone Support"},
+        domain:{symbol:"T",data: "Unlimited Free Subdomain"},
+        reports:{symbol:"T",data: "Monthly Status Reports"},
+      }
+    ]
+  
+  }
+  
